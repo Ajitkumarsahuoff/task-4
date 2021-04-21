dfbdd6c04b9792334834b811bd732ae851534f3f
Ajitkumarsahuoff <81662602+Ajitkumarsahuoff@users.noreply.github.com>
2021-04-21 10:05:05 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ a.open('GET','https://restcountries.eu/rest/v2/all',true);
 a.send();
 a.onload=function(){
     var data=JSON.parse(this.response);
-var casia=data.filter((x)=>x.region==='Asia');
+var casia=data.filter((x)=>x.population<200000);
 console.log(casia);
 
     
